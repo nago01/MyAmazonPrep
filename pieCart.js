1a00f2a2c62c96692c26e1d9b509ce115a5b9b8c
nago01 <palashnagwa@gmail.com>
2024-11-30 19:05:47 +0530

    Radio and CheckBox implementation

--- pieCart.js
+++ pieCart.js
@@ -2,10 +2,13 @@ import './style.css';
 function pieCart() {
    document.getElementById('rangeId').addEventListener('input',(e)=>{
         let val = e.target.value;
-        console.log(document.getElementById('piechartId') , 'sdsa');
+        console.log(val, 'val');
+        val = val*3.6;
         //please remember `conic-gradient(red ${val}% , blue 0%);` adding semicolon at end will be wrong
-        document.getElementById('piechartId').style.backgroundImage = `conic-gradient(red ${val}% , blue 0%)`;
+        document.getElementById('piechartId').style.backgroundImage = `conic-gradient(red 0deg, red ${val}deg, blue ${val}deg, blue 360deg)`;
    })
 }
-document.getElementById('piechartId').style.backgroundImage = `conic-gradient(red ${50}% , blue 0%)`;
+let initialval = document.getElementById('rangeId').value;
+initialval = initialval*3.6;
+document.getElementById('piechartId').style.backgroundImage = `conic-gradient(red 0deg, red ${initialval}deg, blue ${initialval}deg, blue 360deg)`;
 pieCart();
---
+++ radioCheckBox.js
@@ -0,0 +1,62 @@
+import './style.css';
+function radioCheckBox() {
+      let name="";
+      let age = 0;
+      let marriedHappy = false;
+      let choices = [];
+      document.getElementById('formId').addEventListener('input',(e)=>{
+         console.log(e,'e.target');
+         let ele = e.target;
+         if(ele.id=="nameId"){
+            name = e.target.value;
+         }
+         else if(ele.id=="ageId"){
+            age = ele.value;
+         }
+         else if(ele.id=="magId1" || ele.id=="magId2"){
+               if(ele.id=="magId1"){
+                marriedHappy = true;
+               }
+               else {
+                marriedHappy = false;
+               }
+         }
+         else {
+            choices.push(ele.id);
+         }
+         console.log(name ,age , marriedHappy , choices);
+      })
+      document.getElementById('formId').addEventListener('submit',(e)=>{
+        console.log('clicked')
+        e.preventDefault();
+        let table = document.getElementById('tableId');
+        let rData = document.createElement('tr');
+        let newName = document.createElement('td');
+        let newAge = document.createElement('td');
+        let newMarriedStatus = document.createElement('td');
+        let newChoices = document.createElement('td');
+        newName.innerText = name;
+        //newName.value = name; this will be wrong
+        newAge.innerText = age;
+        newMarriedStatus.innerText = marriedHappy;
+        newChoices.innerText = choices;
+        rData.appendChild(newName);
+        rData.appendChild(newAge);
+        rData.appendChild(newMarriedStatus);
+        rData.appendChild(newChoices);
+        table.appendChild(rData);
+      })
+}
+
+// function getObject(arr){
+//     let parentArr  = ['choiceId1', 'choiceId2', 'choiceId3'];
+//   let obj = {};
+//   for(let i=0;i<arr.length;i++){
+//     if(parentArr.includes(arr[i])){
+//         obj[arr[i]] = true;
+//     }
+//   }
+//   return obj;
+// }
+
+radioCheckBox();
